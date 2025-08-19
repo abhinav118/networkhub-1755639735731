cff1e59ff4d95ac0b69ed37708b8c3aa800a95c4
Abhi <abhinav118@gmail.com>
2025-08-19 17:42:23 -0400

    Add components/EventCard.tsx

---
+++ components/EventCard.tsx
@@ -0,0 +1,13 @@
+interface EventCardProps {
+  showDate?: any
+  showLocation?: any
+  showAttendees?: any
+}
+
+export default function EventCard({ showDate, showLocation, showAttendees }: EventCardProps) {
+  return (
+    <div className="">
+      {/* EventCard component content */}
+    </div>
+  )
+}
