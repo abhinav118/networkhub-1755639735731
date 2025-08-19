0c0d18ad4374cefaca4652b30ac1699371b27d33
Abhi <abhinav118@gmail.com>
2025-08-19 17:42:23 -0400

    Add components/MessageBubble.tsx

---
+++ components/MessageBubble.tsx
@@ -0,0 +1,12 @@
+interface MessageBubbleProps {
+  alignment?: any
+  timestamp?: any
+}
+
+export default function MessageBubble({ alignment, timestamp }: MessageBubbleProps) {
+  return (
+    <div className="">
+      {/* MessageBubble component content */}
+    </div>
+  )
+}
