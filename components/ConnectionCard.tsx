b175f5fe7f0aded53b9dfb42ebf124383e64a232
Abhi <abhinav118@gmail.com>
2025-08-19 17:42:22 -0400

    Add components/ConnectionCard.tsx

---
+++ components/ConnectionCard.tsx
@@ -0,0 +1,12 @@
+interface ConnectionCardProps {
+  showActions?: any
+  compact?: any
+}
+
+export default function ConnectionCard({ showActions, compact }: ConnectionCardProps) {
+  return (
+    <div className="">
+      {/* ConnectionCard component content */}
+    </div>
+  )
+}
